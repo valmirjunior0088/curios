6492f7b8f9c3f20f6132a57a9d0d51666a724129
Valmir Pretto <valmir.pretto@perimeter.company>
2022-01-14 14:12:51 -0300

    Remove unnecessary space

--- src/Curios/Source/Parser.hs
+++ src/Curios/Source/Parser.hs
@@ -109,8 +109,8 @@ psLiteral =
 psOperation :: Parser Operation
 psOperation =
   psLexeme parser where
-    opInt32Sum = OpInt32Sum <$> getSourcePos <* psSymbol "int32_sum "
-    opFlt32Sum = OpFlt32Sum <$> getSourcePos <* psSymbol "flt32_sum "
+    opInt32Sum = OpInt32Sum <$> getSourcePos <* psSymbol "int32_sum"
+    opFlt32Sum = OpFlt32Sum <$> getSourcePos <* psSymbol "flt32_sum"
     parser = try opInt32Sum <|> opFlt32Sum
 
 psClosed :: Parser Expression
