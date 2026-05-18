af06475289e21a8561c6e0bbe9f4ecbcdd869fe7
Valmir Pretto <valmirjunior0088@gmail.com>
2026-05-18 19:59:42 -0300

    More examples

---
+++ examples/binary_tree.rs
@@ -0,0 +1,45 @@
+use {
+    curios::{cont, core, ersd},
+    std::time::Duration,
+};
+
+fn main() {
+
+    let term = "
+        rec Tree : Type = {
+            label : '[leaf, node],
+            match label : _ => Type;
+            | 'leaf => Int;
+            | 'node => {Int, Tree, Tree}; };
+        rec sum : Tree -> Int = t =>
+            split t : _ => Int; | (label, value) =>
+            match label : _ => Int;
+            | 'leaf => value;
+            | 'node =>
+                split value : _ => Int; | (v, left, right) =>
+                Int.add v (Int.add (sum left) (sum right));;
+        let tree : Tree =
+            ('node, (1i,
+                ('node, (2i, ('leaf, 3i), ('leaf, 4i))),
+                ('node, (5i, ('leaf, 6i), ('leaf, 7i)))));
+        sum tree
+        "
+    .parse()
+    .expect("expected core term");
+
+    let cont_module = ersd::to_cont(
+        &core::erase(
+            &mut core::Context::new(Duration::from_secs(5)),
+            &term,
+            &"Int".parse().expect("expected result type"),
+        )
+        .expect("expected erased term"),
+    );
+
+    println!("=== cont ===");
+    println!("{cont_module}");
+
+    println!();
+    println!("=== wasm ===");
+    println!("{}", cont::to_wasm(&cont_module));
+}
---
+++ examples/fibonacci.rs
@@ -0,0 +1,36 @@
+use {
+    curios::{cont, core, ersd},
+    std::time::Duration,
+};
+
+fn main() {
+
+    let term = "
+        rec fib_pair : Nat -> {Int, Int} = n =>
+            Nat.match n : _ => {Int, Int};
+            | 0n => (0i, 1i);
+            | pred ih =>
+                split ih : _ => {Int, Int}; | (a, b) =>
+                (b, Int.add a b);;
+        split fib_pair 10n : _ => Int; | (a, b) =>
+        a
+        "
+    .parse()
+    .expect("expected core term");
+
+    let cont_module = ersd::to_cont(
+        &core::erase(
+            &mut core::Context::new(Duration::from_secs(5)),
+            &term,
+            &"Int".parse().expect("expected result type"),
+        )
+        .expect("expected erased term"),
+    );
+
+    println!("=== cont ===");
+    println!("{cont_module}");
+
+    println!();
+    println!("=== wasm ===");
+    println!("{}", cont::to_wasm(&cont_module));
+}
