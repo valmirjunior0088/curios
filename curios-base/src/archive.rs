3193ccf747805207e11696b9b006cd3f09e88960
Valmir Pretto <valmirjunior0088@gmail.com>
2026-08-08 14:11:15 -0300

    Fold the twin big-integer rkyv adapters onto one declaring macro

--- curios-base/src/archive.rs
+++ curios-base/src/archive.rs
@@ -9,72 +9,52 @@ use {
     },
 };
 
-pub struct BigUintBytes;
+/// Declares one with-adapter archiving `$int` as the little-endian byte vector `$to` produces and `$from` reads back. The twins below differ only in these parameters, so a change to the adapter shape reaches both.
+macro_rules! big_bytes {
+    ($adapter:ident, $int:ident, $to:ident, $from:ident) => {
+        pub struct $adapter;
 
-impl ArchiveWith<BigUint> for BigUintBytes {
-    type Archived = Archived<Vec<u8>>;
-    type Resolver = Resolver<Vec<u8>>;
+        impl ArchiveWith<$int> for $adapter {
+            type Archived = Archived<Vec<u8>>;
+            type Resolver = Resolver<Vec<u8>>;
 
-    fn resolve_with(value: &BigUint, resolver: Self::Resolver, out: Place<Self::Archived>) {
-        value.to_bytes_le().resolve(resolver, out);
-    }
+            fn resolve_with(value: &$int, resolver: Self::Resolver, out: Place<Self::Archived>) {
+                value.$to().resolve(resolver, out);
+            }
+        }
+
+        impl<S> SerializeWith<$int, S> for $adapter
+        where
+            S: Fallible + ?Sized,
+            Vec<u8>: Serialize<S>,
+        {
+            fn serialize_with(
+                value: &$int,
+                serializer: &mut S,
+            ) -> Result<Self::Resolver, S::Error> {
+                value.$to().serialize(serializer)
+            }
+        }
+
+        impl<D> DeserializeWith<Archived<Vec<u8>>, $int, D> for $adapter
+        where
+            D: Fallible + ?Sized,
+            Archived<Vec<u8>>: Deserialize<Vec<u8>, D>,
+        {
+            fn deserialize_with(
+                value: &Archived<Vec<u8>>,
+                deserializer: &mut D,
+            ) -> Result<$int, D::Error> {
+                Ok($int::$from(&value.deserialize(deserializer)?))
+            }
+        }
+    };
 }
 
-impl<S> SerializeWith<BigUint, S> for BigUintBytes
-where
-    S: Fallible + ?Sized,
-    Vec<u8>: Serialize<S>,
-{
-    fn serialize_with(value: &BigUint, serializer: &mut S) -> Result<Self::Resolver, S::Error> {
-        value.to_bytes_le().serialize(serializer)
-    }
-}
-
-impl<D> DeserializeWith<Archived<Vec<u8>>, BigUint, D> for BigUintBytes
-where
-    D: Fallible + ?Sized,
-    Archived<Vec<u8>>: Deserialize<Vec<u8>, D>,
-{
-    fn deserialize_with(
-        value: &Archived<Vec<u8>>,
-        deserializer: &mut D,
-    ) -> Result<BigUint, D::Error> {
-        Ok(BigUint::from_bytes_le(&value.deserialize(deserializer)?))
-    }
-}
-
-pub struct BigIntBytes;
-
-impl ArchiveWith<BigInt> for BigIntBytes {
-    type Archived = Archived<Vec<u8>>;
-    type Resolver = Resolver<Vec<u8>>;
-
-    fn resolve_with(value: &BigInt, resolver: Self::Resolver, out: Place<Self::Archived>) {
-        value.to_signed_bytes_le().resolve(resolver, out);
-    }
-}
-
-impl<S> SerializeWith<BigInt, S> for BigIntBytes
-where
-    S: Fallible + ?Sized,
-    Vec<u8>: Serialize<S>,
-{
-    fn serialize_with(value: &BigInt, serializer: &mut S) -> Result<Self::Resolver, S::Error> {
-        value.to_signed_bytes_le().serialize(serializer)
-    }
-}
-
-impl<D> DeserializeWith<Archived<Vec<u8>>, BigInt, D> for BigIntBytes
-where
-    D: Fallible + ?Sized,
-    Archived<Vec<u8>>: Deserialize<Vec<u8>, D>,
-{
-    fn deserialize_with(
-        value: &Archived<Vec<u8>>,
-        deserializer: &mut D,
-    ) -> Result<BigInt, D::Error> {
-        Ok(BigInt::from_signed_bytes_le(
-            &value.deserialize(deserializer)?,
-        ))
-    }
-}
+big_bytes!(BigUintBytes, BigUint, to_bytes_le, from_bytes_le);
+big_bytes!(
+    BigIntBytes,
+    BigInt,
+    to_signed_bytes_le,
+    from_signed_bytes_le
+);
